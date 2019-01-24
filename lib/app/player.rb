9a85a269480d7f35e9f0eccdf29b9064b1357cf5
Modeuil <votre_email@votre_email.com>
2019-01-24 11:28:13 +0100

    modification player.rb class create

---
+++ lib/app/player.rb
@@ -0,0 +1,27 @@
+
+
+class Player
+	@name= ""
+	@number_of_win = 0
+	@number_of>_egality = 0
+
+	def initialize(game)
+		@name = name
+	end
+
+	def return_name
+
+	end
+
+	def increments_number_of_win
+
+	end
+
+	def increments_number_of_egality
+
+	end
+
+	def return_number_of_win
+
+	end
+end
