33a5a9a5a842cd9ea99d97926600e9af3d74e6b1
Amine42 <amine.bouraghda@gmail.com>
2019-01-24 15:46:11 +0100

    class game

--- lib/app/player.rb
+++ lib/app/player.rb
@@ -6,8 +6,8 @@ class Player
 	@number_of_win = 0
 	@number_of_egality = 0
 
-	def initialize(name)
-		print "name of gamer : "
+	def initialize
+		# print "name of gamer : "
 		@name = gets.chomp
 	end
 
