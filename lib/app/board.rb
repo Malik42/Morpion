a7d849d998f0c6c98644166cb4c39b82bd3b9750
Malik42 <malik.ahres@hotmail.fr>
2019-01-24 21:18:50 +0100

    colorize

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -1,3 +1,4 @@
+require 'colorize'
 # Classe Board : La class Board sert à afficher
 #
 # Variable : Possède aucune variable
@@ -12,18 +13,18 @@ class Board
 
   def display_board(hash)
     puts "      1       2       3"
-    puts "  *************************"
-    puts "  *       |       |       *"
-    puts "A *   #{hash.fetch("A1")}   |   #{hash.fetch("A2")}   |   #{hash.fetch("A3")}   *"
-    puts "  *       |       |       *"
-    puts "  *-----------------------*"
-    puts "  *       |       |       *"
-    puts "B *   #{hash.fetch("B1")}   |   #{hash.fetch("B2")}   |   #{hash.fetch("B3")}   *"
-    puts "  *       |       |       *"
-    puts "  *-----------------------*"
-    puts "  *       |       |       *"
-    puts "C *   #{hash.fetch("C1")}   |   #{hash.fetch("C2")}   |   #{hash.fetch("C3")}   *"
-    puts "  *       |       |       *"
-    puts "  *************************"
+    puts "  *************************".colorize(:blue)
+    puts "  *       |       |       *".colorize(:blue)
+    puts "A *   #{hash.fetch("A1")}   |   #{hash.fetch("A2")}   |   #{hash.fetch("A3")}   *".colorize(:blue)
+    puts "  *       |       |       *".colorize(:blue)
+    puts "  *-----------------------*".colorize(:blue)
+    puts "  *       |       |       *".colorize(:blue)
+    puts "B *   #{hash.fetch("B1")}   |   #{hash.fetch("B2")}   |   #{hash.fetch("B3")}   *".colorize(:blue)
+    puts "  *       |       |       *".colorize(:blue)
+    puts "  *-----------------------*".colorize(:blue)
+    puts "  *       |       |       *".colorize(:blue)
+    puts "C *   #{hash.fetch("C1")}   |   #{hash.fetch("C2")}   |   #{hash.fetch("C3")}   *".colorize(:blue)
+    puts "  *       |       |       *".colorize(:blue)
+    puts "  *************************".colorize(:blue)
   end
 end
