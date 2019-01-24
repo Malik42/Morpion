a6f29e05cc27fd84beb213c8740ea36f66c9702a
Amine42 <amine.bouraghda@gmail.com>
2019-01-24 13:37:27 +0100

    conflict

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -1,5 +1,4 @@
 class Board
-<<<<<<< HEAD
   def display_board(hash)
     puts "       |       |"
     puts "   #{hash.fetch("A1")}   |   #{hash.fetch("A2")}   |   #{hash.fetch("A3")}"
@@ -13,9 +12,6 @@ class Board
     puts "   #{hash.fetch("C1")}   |   #{hash.fetch("C2")}   |   #{hash.fetch("C3")}"
     puts "       |       |"
   end
-=======
-  def display_board
->>>>>>> 8eaac2d9b0cc77f8124de660d433fe5e35202372
 
   def puts_hello
     puts "hello"
