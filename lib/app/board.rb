3d5762351f0b3f97fa7f35bee92e0d3dd03911db
Amine42 <amine.bouraghda@gmail.com>
2019-01-24 11:29:10 +0100

    board skeleton create

---
+++ lib/app/board.rb
@@ -0,0 +1,12 @@
+class board
+  @borad_case = {"A1" => " ", "A2" => " ", "A3" => " ", "B1" => " ", "B2" => " ", "B3" => " ", "C1" => " ", "C2" => " ", "C3" => " ", }
+
+  def display_board
+
+  end
+
+  def send_board_case
+
+  end
+
+end
