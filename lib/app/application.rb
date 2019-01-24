c97265ce4fc250d7ebc6e9d3060a878a52ac94a8
Malik42 <malik.ahres@hotmail.fr>
2019-01-24 11:27:03 +0100

    squelet class application.rb finish

---
+++ lib/app/application.rb
@@ -0,0 +1,23 @@
+class application
+	@number_of_part = ""
+
+	def initialize
+
+	end
+
+	def start_game
+
+	end
+
+	def incrementes_number_of_part
+
+	end
+
+	def ask_replay
+
+	end
+
+	def exit_game
+		
+	end
+end
